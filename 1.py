057b0e4de67539aec71419282f89f6d3bfe878a4
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2024-11-27 19:01:20 +0530

    change p

---
+++ 1.py
@@ -0,0 +1,24 @@
+def draw_heart():
+    heart = """
+       ****     ****
+     **    ** **    **
+    **       *       **
+    **               **
+     **             **
+       **         **
+         **     **
+           ** **
+            ***
+             *
+    """
+    print(heart)
+
+def display_text():
+    print("\n    Abhishek kumar\n")
+
+# Draw the heart
+draw_heart()
+
+# Display the text
+display_text()
+
